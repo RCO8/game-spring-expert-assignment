9ee626bb4b1192d3aa2cf8485743ba76078967fa
RCO8 <gurwo63@naver.com>
2026-09-16 17:48:10 +0900

    컴포넌트 스캔 제거

--- src/main/java/com/gameexpert/GameExpertApplication.java
+++ src/main/java/com/gameexpert/GameExpertApplication.java
@@ -9,9 +9,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.CUSTOM,
+/*@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.CUSTOM,
         classes = {EngineComponentFilter.class, TypeExcludeFilter.class,
-                AutoConfigurationExcludeFilter.class}))
+                AutoConfigurationExcludeFilter.class}))*/ // 이건 스프링부트 앱 어노테이션 안에 있기 때문에 따로 있으면 안된다.
 public class GameExpertApplication {
     public static void main(String[] args) {
         SpringApplication.run(GameExpertApplication.class, args);
